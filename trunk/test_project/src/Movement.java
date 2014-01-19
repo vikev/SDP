182b98b5649fbb74d9beeb5038e56989563d3bbb
Borislav Ikonomov <s1101271@lappy.inf.ed.ac.uk>
2014-01-19 18:31:00 +0000

    Actually adding changed code from last commit - I'm an idiot

--- trunk/test_project/src/Movement.java
+++ trunk/test_project/src/Movement.java
@@ -1,33 +1,47 @@
+package fromRepo;
+import lejos.nxt.NXTRegulatedMotor; // Switch to using this instead of Motor
 import lejos.nxt.Motor;
 import lejos.robotics.navigation.DifferentialPilot;
-import lejos.nxt.TouchSensor;
-import lejos.nxt.SensorPort;
 
 public class Movement {
 	DifferentialPilot pilot;
-	TouchSensor stop;
 
 	public Movement() {
-		pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.C, true);
+		pilot = new DifferentialPilot(2.25f, 5.5f,(NXTRegulatedMotor) Motor.A,(NXTRegulatedMotor) Motor.C, true);
 	}
 
 	public void turn() {
 
 	}
-
+	
+	// TODO Needs finer grained (maybe faster) left and right turns
+	// TODO Refactor
 	public void driveForward() {
-		while (!stop.isPressed()) {
+		while (true) {
 			System.out.println("Forward");
-			pilot.setRotateSpeed(0.5);
-			pilot.forward();
-			while (ColorScanner.isOnWhite()) {
-				pilot.stop();
+			pilot.setRotateSpeed(200);
+			pilot.setTravelSpeed(8);
+			while (ColorScanner.getNewDirection()==Direction.FORWARD) {
+				pilot.forward();
 			}
 			switch(ColorScanner.getNewDirection()){
-				case BACKWARD: pilot.rotate(180); System.out.println("Backward");break;
-				case LEFT: pilot.rotate(90); System.out.println("Left"); break;
-				case RIGHT: pilot.rotate(-90); System.out.println("Right");break;
+				case BACKWARD: 
+					System.out.println("Backward");
+					pilot.setTravelSpeed(5);
+					while (ColorScanner.getNewDirection()!=Direction.BACKWARD) {
+						pilot.backward();
+					}
+					break;
+				case LEFT: 
+					System.out.println("Left");
+					pilot.rotate(5);
+					break;
+				case RIGHT:
+					System.out.println("Right");
+					pilot.rotate(-5);
+					break;
 			}
+		}
 	}
 
 	public static void main(String args[]) {
