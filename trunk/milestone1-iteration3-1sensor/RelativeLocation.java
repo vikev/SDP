2829dc467ab86d948501daa66b966d94099997ab
Blake Hawkins <Cokemonkey11@gmail.com>
2014-01-20 19:47:41 +0000

    created RelativeLocation file, which will serve as a backbone for the second part of the milestone1 (i hope)

---
+++ trunk/milestone1-iteration3-1sensor/RelativeLocation.java
@@ -0,0 +1,35 @@
+
+// Needs proper files imported - NOT been debugged or tested
+// author s1143704
+
+public class RelativeLocation{
+    private static final double FRAME_ANGLE = 1.0 //To be calculated
+    private static final double DEGTORAD = 
+    public static double x = 0.0, y = 0.0, facing = 0.0;
+    
+    public static void addAng(double deg){
+        facing+=deg;
+    }
+    
+    public static void addAng(){ //this method adds the ang turned in one FRAME, we'll have to
+                                 //calculate this ourselves
+        facing+=FRAME_ANGLE;
+    }
+    
+    public static void subAng(){
+        facing-=FRAME_ANGLE;
+    }
+    
+    public static void addDist(double cm){
+        x += cm * Math.cos(facing*DEGTORAD);
+        y += cm * Math.sin(facing*DEGTORAD);
+    }
+    
+    public static void returnAng(){
+        //Motor.rotateTo(facing+180.);
+    }
+    
+    public static void returnDist(){
+        //Motor.move(Math.sqrt(x*x+y*y));
+    }
+}
