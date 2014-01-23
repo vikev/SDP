277d2bf5d275d42496314310d1ae6f41af5367d1
vikev <lubomirvikev@gmail.com>
2014-01-23 17:30:55 +0000

    kickergit add -u! yey

--- trunk/PC/PCControl/src/sdp/pc/Start.java
+++ trunk/PC/PCControl/src/sdp/pc/Start.java
@@ -8,27 +8,19 @@ import lejos.pc.comm.NXTInfo;
 
 public class Start {
 	public static void main(String[] args) {
-		NXTInfo nxt1 = new NXTInfo(NXTCommFactory.BLUETOOTH, "SDP 9B",
-				"001653077531");
+		//NXTInfo nxt1 = new NXTInfo(NXTCommFactory.BLUETOOTH, "SDP 9B",
+		//		"001653077531");
 
-//		 NXTInfo nxt1 = new NXTInfo(NXTCommFactory.BLUETOOTH, "SDP 9A",
-//					"0016530BB5A3");
+		 NXTInfo nxt1 = new NXTInfo(NXTCommFactory.BLUETOOTH, "SDP 9A",
+					"0016530BB5A3");
 
 		BTConnection conn1 = new BTConnection(nxt1, NXTComm.PACKET);
 
 		final Driver driver1 = new Driver(conn1);
 
-		
-		driver1.forward(50);
-		driver1.turnLeft(90);
-		driver1.forward(50);
-		driver1.turnLeft(90);
-		driver1.forward(50);
-		driver1.turnLeft(90);
-		driver1.forward(50);
-		driver1.turnLeft(90);
-		
-		
+		driver1.forward();
+		driver1.kick(-90);
+		driver1.forward(1);
 		
 		conn1.disconnect();
 //		conn2.disconnect();
