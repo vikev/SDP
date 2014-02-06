ed431f52e112f0d21100177ac294cab6088be7eb
Blake Hawkins <Cokemonkey11@gmail.com>
2014-02-06 15:08:29 +0000

    Improved documentation marginally.

--- trunk/PC/PCControl/src/sdp/pc/milestones/Milestone3att.java
+++ trunk/PC/PCControl/src/sdp/pc/milestones/Milestone3att.java
@@ -1,6 +1,5 @@
 package sdp.pc.milestones;
 
-
 import sdp.pc.vision.Point2;
 import sdp.pc.vision.Vision;
 import sdp.pc.vision.WorldState;
@@ -9,8 +8,18 @@ import sdp.pc.vision.relay.TCPClient;
 import sdp.pc.common.*;
 
 /**
- * This class should be re-implemented to use ChooseRobot and TCPClient like
- * code instead of Driver.
+ * This class contains a main method for executing the attacker perspective in
+ * Milestone 3. The implementation will order the robot to navigate to the ball
+ * from an offensive angle and shoot for the defending goal.
+ * 
+ * To run M3att, first pair the machine/robot bluetooth connection. Then
+ * navigate to /SDP/trunk/PC/PCControl/ in a terminal and execute ant with the
+ * command 'ant'; select defender ('d') during ant build. Run
+ * milestone3att.java; select defender again from eclipse ('d'). Calibrate the
+ * vision feed by clicking borders.
+ * 
+ * Note: This class should be re-implemented to use ChooseRobot and TCPClient
+ * like code instead of Driver.
  * 
  * @author s1117764
  * 
@@ -209,56 +218,62 @@ public class Milestone3att {
 			rotateBy = robotOrientation - targetAngle;
 		}
 	}
-	
-	/*Pseudo Code for getting the robot to navigate to, and kick, the ball.
-	 * Call chooseRobot
-	 * connect to chosen Robot
-	 * create Vision instance
-	 * discern target goal
-	 * 	assign target point within goal (currently only use goal centre) - separate method
-	 * 	create vectors for line ball to goal and for ball to robot using ball position as (0,0) point of coordinate axis
-	 * 	assign ball's approach point
-	 * 	if ball's approach point lies within selected robots bounding box then
-	 * 		if angle between line connecting ball to the target goal and the line connecting the ball to the Robot is >90 degrees and <270 
-	 * 			move to the ball's approach point
-	 * 			face target goal and approach and kick ball 
-	 * 		else 
-	 * 			set way-point 90 degrees or 270 degrees to the line connecting ball to goal (whichever is closer to the angle between line connecting ball to the target goal and the line connecting the ball to the Robot)
-	 * 			if way-point is within robots bounding box 
-	 * 				go to way-point
-	 * 				face and move to the ball's approach point
-	 * 				face target goal and approach and kick ball 
-	 * 			else
-	 * 				Robot is stuck in a corner of its bounding box and can't kick the ball towards the goal.
-	 * 				display that shot has been deemed impossible
-	 * 	else
-	 * 		display that shot has been deemed impossible
-	 * 	end
+
+	/*
+	 * Pseudo Code for getting the robot to navigate to, and kick, the ball.
+	 * Call chooseRobot connect to chosen Robot create Vision instance discern
+	 * target goal assign target point within goal (currently only use goal
+	 * centre) - separate method create vectors for line ball to goal and for
+	 * ball to robot using ball position as (0,0) point of coordinate axis
+	 * assign ball's approach point if ball's approach point lies within
+	 * selected robots bounding box then if angle between line connecting ball
+	 * to the target goal and the line connecting the ball to the Robot is >90
+	 * degrees and <270 move to the ball's approach point face target goal and
+	 * approach and kick ball else set way-point 90 degrees or 270 degrees to
+	 * the line connecting ball to goal (whichever is closer to the angle
+	 * between line connecting ball to the target goal and the line connecting
+	 * the ball to the Robot) if way-point is within robots bounding box go to
+	 * way-point face and move to the ball's approach point face target goal and
+	 * approach and kick ball else Robot is stuck in a corner of its bounding
+	 * box and can't kick the ball towards the goal. display that shot has been
+	 * deemed impossible else display that shot has been deemed impossible end
 	 * 
-	 * Throughout this loop if ball location is changed then loop should start from the top again after a period of waiting for the ball's reported position to stabilise
-	 * */
-	
-	public static void kickball (WorldState state, TCPClient robotControl, Vision vision, int chosenRobot){
+	 * Throughout this loop if ball location is changed then loop should start
+	 * from the top again after a period of waiting for the ball's reported
+	 * position to stabilise
+	 */
+
+	@SuppressWarnings("unused")
+	public static void kickball(WorldState state, TCPClient robotControl,
+			Vision vision, int chosenRobot) {
 		int targetGoal = state.getTargetGoal();
 		if (chosenRobot == Constants.ATTACKER)
 			chosenRobot = 1;
-		if(chosenRobot == Constants.DEFENDER)
+		if (chosenRobot == Constants.DEFENDER)
 			chosenRobot = 0;
 		Point2 targetPoint = setBallTarget(state);
-		if (targetPoint.getX() == 0 & targetPoint.getY() == 0){
-			System.out.println("Could not assign ball target point within target goal");
-			return;		//could not set target point for ball and therefore cannot take shot
+		if (targetPoint.getX() == 0 & targetPoint.getY() == 0) {
+			System.out
+					.println("Could not assign ball target point within target goal");
+			return; // could not set target point for ball and therefore cannot
+					// take shot
 		}
-		int[] vectorLineBalltoTargetPoint = {(state.getBallPosition().getX() - targetPoint.getX()), (state.getBallPosition().getY() - targetPoint.getY())};
-		int[] vectorLineBalltoSelectedRobot = {(state.getBallPosition().getX() - state.getRobotPosition(0, chosenRobot).getX()), (state.getBallPosition().getY() - state.getRobotPosition(0, chosenRobot).getY())};
-	
+		int[] vectorLineBalltoTargetPoint = {
+				(state.getBallPosition().getX() - targetPoint.getX()),
+				(state.getBallPosition().getY() - targetPoint.getY()) };
+		int[] vectorLineBalltoSelectedRobot = {
+				(state.getBallPosition().getX() - state.getRobotPosition(0,
+						chosenRobot).getX()),
+				(state.getBallPosition().getY() - state.getRobotPosition(0,
+						chosenRobot).getY()) };
+
 	}
-	
-	public static Point2 setBallTarget(WorldState state){
-		if(1 == state.getTargetGoal())	//if target goal is the right hand goal
+
+	public static Point2 setBallTarget(WorldState state) {
+		if (1 == state.getTargetGoal()) // if target goal is the right hand goal
 			return state.getRightGoalCentre();
-		if(0 == state.getTargetGoal())
+		if (0 == state.getTargetGoal())
 			return state.getLeftGoalCentre();
-		return new Point2(0,0);
+		return new Point2(0, 0);
 	}
 }
