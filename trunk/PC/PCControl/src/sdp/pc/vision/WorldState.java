b54798607711c526d0c9a49fa2efc4fbff123854
Blake Hawkins <Cokemonkey11@gmail.com>
2014-02-10 17:59:53 +0000

    Documentation improvements to WorldState.java

--- trunk/PC/PCControl/src/sdp/pc/vision/WorldState.java
+++ trunk/PC/PCControl/src/sdp/pc/vision/WorldState.java
@@ -8,15 +8,49 @@ import sdp.pc.common.Constants;
  * 
  */
 public class WorldState {
-	public static final int PLAYERS_PER_TEAM = 2, TEAM_COUNT = 2;
 
-	// Next three might need an enum, though we won't really need to ever touch
-	// them here
-	private int pitch; // 0 -> Main Pitch; 1 -> Side Pitch
-	private int ourColor; // 0 -> Yellow; 1 -> Blue
-	private int shootingDirection; // 0 -> Left; 1 -> Right
+	/**
+	 * Simple value for the number of players per team. TODO: Should probably be
+	 * moved to a global constants class to avoid code duplication.
+	 */
+	public static final int PLAYERS_PER_TEAM = 2;
 
+	/**
+	 * Simple value for the number of teams. TODO: Should probably be moved to a
+	 * global constants class to avoid code duplication.
+	 */
+	public static final int TEAM_COUNT = 2;
+
+	/**
+	 * The pitch associated with this instance of WorldState. 0 for Main, 1 for
+	 * side (TODO: Should be abstracted)
+	 */
+	private int pitch;
+
+	/**
+	 * The colour of our team in this instance of WorldState. 0 for Yellow, 1
+	 * for Blue. (TODO: Should be abstracted)
+	 */
+	private int ourColor;
+
+	/**
+	 * Which direction our robots are aiming for in this instance of WorldState.
+	 * 0 for left, 1 for right. (TODO: Should be abstracted)
+	 */
+	private int shootingDirection;
+
+	/**
+	 * The best known location of the ball in <b>this</b>. Is reset to
+	 * Point2.EMPTY when position is unknown.
+	 */
 	private Point2 ballLocation = new Point2();
+
+	/**
+	 * The best known velocity of the ball in <b>this</b>. It is reset to
+	 * Point2.EMPTY when unknown. Note that Point2 is used because x,y refer to
+	 * the del properties of the ball (Difference in x and y of the ball from
+	 * frame to frame)
+	 */
 	private Point2 ballVelocity = new Point2();
 
 	/**
@@ -31,26 +65,70 @@ public class WorldState {
 	 */
 	private Point2 estimatedCollidePoint = new Point2();
 
-	private double ballFacing, ballSpeed;
+	/**
+	 * The movement angle of the ball in degrees
+	 */
+	private double ballFacing;
+
+	/**
+	 * The movement speed of the ball TODO: Units? Should likely use pixels per
+	 * second.
+	 */
+	private double ballSpeed;
+
+	/**
+	 * Set of robot locations (Reset to Point2.EMPTY if a hat is not found)
+	 */
 	private Point2[][] robotLoc = new Point2[TEAM_COUNT][PLAYERS_PER_TEAM];
+
+	/**
+	 * Set of robot facing angles TODO: Units? should be degrees on [0,360).
+	 * What is it set to if the robot position is unknown?
+	 */
 	private double[][] robotFacing = new double[TEAM_COUNT][PLAYERS_PER_TEAM];
 
-	// Those are assigned on start either from default value or config file
+	/**
+	 * Goal points established using calibration values in the settings GUI.
+	 */
 	public static Point2 leftGoalTop;
+
+	/**
+	 * Goal points established using calibration values in the settings GUI.
+	 */
 	public static Point2 leftGoalBottom;
+
+	/**
+	 * Goal points established using calibration values in the settings GUI.
+	 */
 	public static Point2 rightGoalTop;
+
+	/**
+	 * Goal points established using calibration values in the settings GUI.
+	 */
 	public static Point2 rightGoalBottom;
+
+	/**
+	 * Goal points calculated using calibration values from the settings GUI.
+	 */
 	public static Point2 leftGoalCentre;
+
+	/**
+	 * Goal points calculated using calibration values from the settings GUI.
+	 */
 	public static Point2 rightGoalCentre;
 
+	/**
+	 * Simple constructor which sets robot positions to empty.
+	 */
 	public WorldState() {
-
-		// Initialise robot locations to null values
 		for (int t = 0; t < TEAM_COUNT; t++)
 			for (int p = 0; p < PLAYERS_PER_TEAM; p++)
 				robotLoc[t][p] = new Point2();
 	}
 
+	/**
+	 * Reset method for goal points.
+	 */
 	public static void resetGoal() {
 		leftGoalTop = null;
 		leftGoalBottom = null;
@@ -60,25 +138,46 @@ public class WorldState {
 		rightGoalCentre = null;
 	}
 
+	/**
+	 * Getter method for ball facing
+	 * 
+	 * @return TODO: Returns ball facing in degrees on [0,360)? (It should)
+	 */
 	public double getBallFacing() {
 		return ballFacing;
 	}
 
+	/**
+	 * Getter method for ball speed
+	 * 
+	 * @return TODO: Ball speed in pixels per second? Or per frame? To discuss.
+	 */
 	public double getBallSpeed() {
 		return ballSpeed;
 	}
 
-	// const getters
+	/**
+	 * Getter method for left goal centre
+	 * 
+	 * @return Left Goal Centre as a Point2
+	 */
 	public Point2 getLeftGoalCentre() {
 		return Constants.LEFT_GOAL_CENTRE;
 	}
 
+	/**
+	 * Getter method for right goal centre
+	 * 
+	 * @return Right Goal Centre as a Point2
+	 */
 	public Point2 getRightGoalCentre() {
 		return Constants.RIGHT_GOAL_CENTRE;
 	}
 
 	/**
-	 * Gets the current position of the ball
+	 * Getter method for ball position
+	 * 
+	 * @return ball position as a Point2
 	 */
 	public Point2 getBallPosition() {
 		return ballLocation;
@@ -95,10 +194,12 @@ public class WorldState {
 	 * Gets the position of the specified robot
 	 * 
 	 * @param team
-	 *            the team of the robot, 0 for yellow and 1 for blue
+	 *            the team of the robot, 0 for yellow and 1 for blue TODO:
+	 *            Should be abstracted
 	 * @param robot
-	 *            the id of the robot, 0 for left one and 1 foe the right one
-	 * @return the position of the robot
+	 *            the id of the robot, 0 for left one and 1 for the right one
+	 *            TODO: Should be abstracted
+	 * @return the position of the requested robot as a Point2
 	 */
 	public Point2 getRobotPosition(int team, int robot) {
 		return robotLoc[team][robot];
@@ -125,7 +226,7 @@ public class WorldState {
 	 *            the team of the robot, 0 for yellow and 1 for blue
 	 * @param robot
 	 *            the id of the robot, 0 for left one and 1 for the right one
-	 * @return the orientation of the robot, in ???
+	 * @return the orientation of the robot, in TODO:
 	 */
 	public double getRobotFacing(int team, int robot) {
 		return robotFacing[team][robot];
@@ -139,35 +240,37 @@ public class WorldState {
 	 * @param robot
 	 *            - the robot to update.
 	 * @param newFacing
-	 *            - the updated angle of facing for the robot.
+	 *            - the updated angle of facing for the robot. TODO: Units
 	 */
 	public void setRobotFacing(int team, int robot, double newFacing) {
 		this.robotFacing[team][robot] = newFacing;
 	}
 
 	/**
-	 * Set which pitch we're playing on (0 is Main, 1 is Side)
+	 * Set which pitch we're playing on (0 is Main, 1 is Side) TODO: Should be
+	 * abstracted
 	 */
 	public void setPitch(int pitch) {
 		this.pitch = pitch;
 	}
 
 	/**
-	 * Get which pitch we're playing on (0 is Main, 1 is Side)
+	 * Get which pitch we're playing on (0 is Main, 1 is Side) TODO: Should be
+	 * abstracted
 	 */
 	public int getPitch() {
 		return pitch;
 	}
 
 	/**
-	 * Set our team's colour (0 is yellow, 1 is blue)
+	 * Set our team's colour (0 is yellow, 1 is blue) TODO: Should be abstracted
 	 */
 	public void setOurColor(int ourColor) {
 		this.ourColor = ourColor;
 	}
 
 	/**
-	 * Get our team's colour (0 is yellow, 1 is blue)
+	 * Get our team's colour (0 is yellow, 1 is blue) TODO: Should be abstracted
 	 */
 	public int getOurColor() {
 		return ourColor;
@@ -175,6 +278,7 @@ public class WorldState {
 
 	/**
 	 * Gets the direction our team is supposed to shoot towards (0 is left)
+	 * TODO: Should be abstracted
 	 */
 	public int getDirection() {
 		return shootingDirection;
@@ -182,13 +286,14 @@ public class WorldState {
 
 	/**
 	 * Sets the direction our team is supposed to shoot towards (0 is left)
+	 * TODO: Should be abstracted
 	 */
 	public void setDirection(int direction) {
 		this.shootingDirection = direction;
 	}
 
 	/**
-	 * Sets the current velocity of the ball
+	 * Sets the current velocity of the ball TODO: Units
 	 */
 
 	public void setBallVelocity(Point2 ballVelocity) {
@@ -198,7 +303,7 @@ public class WorldState {
 	}
 
 	/**
-	 * Gets the current velocity of the ball
+	 * Gets the current velocity of the ball TODO: Units
 	 */
 
 	public Point2 getBallVelocity() {
@@ -206,35 +311,42 @@ public class WorldState {
 	}
 
 	/**
-	 * Setter method for updating the estimated ball stop point
+	 * Setter method for updating the estimated ball stop point. Used by
+	 * FutureBall
+	 * 
 	 * @param pt
 	 */
 	public void setEstimatedStopPoint(Point2 pt) {
 		this.estimatedStopLocation = pt;
 	}
-	
+
 	/**
-	 * Setter method for updating the estimated collide point
+	 * Setter method for updating the estimated collide point. Used by
+	 * FutureBall
+	 * 
 	 * @param pt
 	 */
 	public void setEstimatedCollisionPoint(Point2 pt) {
 		this.estimatedCollidePoint = pt;
 	}
-	
+
 	/**
-	 * Getter method for the estimated stop point of the ball
+	 * Getter method for the estimated stop point of the ball. Used by
+	 * FutureBall
+	 * 
 	 * @return
 	 */
-	public Point2 getEstimatedStopPoint(){
+	public Point2 getEstimatedStopPoint() {
 		return this.estimatedStopLocation;
 	}
-	
+
 	/**
-	 * Getter method for the estimated collide point of the ball
+	 * Getter method for the estimated collide point of the ball. Used by
+	 * FutureBall
+	 * 
 	 * @return
 	 */
-	public Point2 getEstimatedCollidePoint(){
+	public Point2 getEstimatedCollidePoint() {
 		return this.estimatedCollidePoint;
 	}
-
 }
