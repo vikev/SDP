280a3ff8d40ed4312016eee4a1e22a76a373215e
vikev <lubomirvikev@gmail.com>
2014-02-10 18:37:00 +0000

    Robot now would move at the speed given.

--- trunk/NXJ/BluetoothReceiver/src/sdp/nxj/bluetooth/DiffPilot.java
+++ trunk/NXJ/BluetoothReceiver/src/sdp/nxj/bluetooth/DiffPilot.java
@@ -8,6 +8,7 @@ public class DiffPilot {
 
 	public DiffPilot() {
 		pilot = new DifferentialPilot(56, 135, Motor.A, Motor.C, true);
+		setRotateSpeed(0);
 	}
 
 	public void driveForward(double distance) {
@@ -17,18 +18,19 @@ public class DiffPilot {
 			pilot.travel(distance, true); // true means command is interrupted
 		} // immediately by next command
 	}
-	
+
 	/**
 	 * Adjusts the pilot's rotation speed to degPerSec
 	 * 
 	 * @param degPerSec
-	 * 			rotate speed in degrees per second
+	 *            rotate speed in degrees per second
 	 */
 	public void setRotateSpeed(double degPerSec) {
 		if (degPerSec == 0) {
 			degPerSec = 900000;
 		}
 		pilot.setRotateSpeed(degPerSec);
+		pilot.setTravelSpeed(degPerSec);
 	}
 
 	public void driveBackward(double distance) {
