022538a32827cb1ccdcdb553b442fd86ac8251b4
Blake Hawkins <Cokemonkey11@gmail.com>
2014-04-01 12:04:53 +0100

    auto-formatted imports for OpCallable.java

--- trunk/PC/PCControl/src/sdp/pc/common/OpCallable.java
+++ trunk/PC/PCControl/src/sdp/pc/common/OpCallable.java
@@ -2,8 +2,6 @@ package sdp.pc.common;
 
 import java.util.concurrent.Callable;
 
-import sdp.pc.common.Parallel.Operation;
-
 public abstract class OpCallable<T> implements Callable<Void> {
 
 	public Iterable<T> elements;
